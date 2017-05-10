2e4a9c50f385fffae44cb9832dbace1e46046976
kamu <kamuxxx@hotmail.com>
2017-05-10 17:28:29 +0900

    Implementing AddText Droid

--- AiForms.Effects.Droid/AddTextPlatformEffect.cs
+++ AiForms.Effects.Droid/AddTextPlatformEffect.cs
@@ -6,7 +6,6 @@ using Android.Widget;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using AGraphics = Android.Graphics;
-using ARelativeLayout = Android.Widget.RelativeLayout;
 
 [assembly: ExportEffect(typeof(AddTextPlatformEffect), nameof(AddText))]
 namespace AiForms.Effects.Droid
@@ -14,29 +13,33 @@ namespace AiForms.Effects.Droid
     public class AddTextPlatformEffect : PlatformEffect
     {
         private TextView textView;
-        private ARelativeLayout relative;
+        private LinearLayout relative;
         private ContainerOnLayoutChangeListener listener;
 
         protected override void OnAttached()
         {
-            relative = new ARelativeLayout(Container.Context);
+            relative = new LinearLayout(Container.Context);
+            relative.Orientation = Orientation.Vertical;
             relative.SetBackgroundColor(Android.Graphics.Color.Red);
 
-            using (var lparam = new ARelativeLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent)) {
-                lparam.AddRule(LayoutRules.AlignParentTop);
-
+            using (var lparam = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent)) {
+                
                 textView = new TextView(Container.Context);
                 textView.SetMaxLines(1);
                 textView.SetMinLines(1);
+                textView.Ellipsize = Android.Text.TextUtils.TruncateAt.End;
+
+                textView.SetBackgroundColor(Android.Graphics.Color.Orange);
 
                 relative.AddView(textView, lparam);
             }
 
             using ( var param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent)) {
+                param.Gravity = GravityFlags.FillVertical | GravityFlags.FillHorizontal | GravityFlags.Bottom;
                 Container.AddView(relative, param);
             }
 
-            listener = new ContainerOnLayoutChangeListener(relative,textView);
+            listener = new ContainerOnLayoutChangeListener(relative,textView,Element);
             Container.AddOnLayoutChangeListener(listener);
 
             UpdateText();
@@ -72,12 +75,14 @@ namespace AiForms.Effects.Droid
             }
             else if(args.PropertyName == AddText.FontSizeProperty.PropertyName){
                 UpdateFontSize();
+                Container.RequestFocus();
             }
             else if(args.PropertyName == AddText.TextColorProperty.PropertyName){
                 UpdateTextColor();
             }
             else if(args.PropertyName == AddText.MarginProperty.PropertyName){
                 UpdateMargin();
+                Container.RequestFocus();
             }
             else if(args.PropertyName == AddText.HorizontalAlignProperty.PropertyName){
                 UpdateHorizontalAlign();
@@ -108,30 +113,23 @@ namespace AiForms.Effects.Droid
         }
 
         void UpdateHorizontalAlign(){
-            textView.Gravity = AddText.GetHorizontalAlign(Element).ToHorizontalGravityFlags();
+            textView.Gravity = AddText.GetHorizontalAlign(Element).ToHorizontalGravityFlags() | AddText.GetVerticalAlign(Element).ToVerticalGravityFlags();
         }
 
         void UpdateVerticalAlign(){
-            var align = AddText.GetVerticalAlign(Element);
-            var param = new ARelativeLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent);
-            param.AddRule(align == Xamarin.Forms.TextAlignment.Start ? LayoutRules.AlignParentTop:LayoutRules.AlignParentBottom);
-            relative.LayoutParameters = param;
-
-            relative.ForceLayout();
-            relative.Invalidate();
-            textView.ForceLayout();
-            textView.Invalidate();
-            //textView.Gravity = align.ToVerticalGravityFlags();
+            Container.RequestLayout();
         }
 
         internal class ContainerOnLayoutChangeListener : Java.Lang.Object, Android.Views.View.IOnLayoutChangeListener
         {
-            private Android.Widget.RelativeLayout _layout;
+            private ViewGroup _layout;
             private TextView _textview;
+            private Element _element;
 
-            public ContainerOnLayoutChangeListener(Android.Widget.RelativeLayout layout,TextView textview) {
+            public ContainerOnLayoutChangeListener(ViewGroup layout,TextView textview,Element element) {
                 _layout = layout;
                 _textview = textview;
+                _element = element;
             }
 
             //ContainerにAddViewした子要素のサイズを確定する必要があるため
@@ -139,8 +137,14 @@ namespace AiForms.Effects.Droid
             public void OnLayoutChange(Android.Views.View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
                 _layout.Right = v.Right;
                 _layout.Bottom = v.Bottom;
+
                 _textview.Right = v.Right;
-                _textview.Bottom = 30;
+
+                var height = (int)Xamarin.Forms.Forms.Context.ToPixels(AddText.GetMargin(_element)) * 2 + _textview.LineHeight;
+                var vpos = AddText.GetVerticalAlign(_element) == Xamarin.Forms.TextAlignment.Start ? 0 : v.Bottom - height;
+
+                _textview.Top = vpos;
+                _textview.Bottom = vpos + height;
             }
         }
 
@@ -153,8 +157,6 @@ namespace AiForms.Effects.Droid
         {
             switch (alignment)
             {
-                case Xamarin.Forms.TextAlignment.Center:
-                    return GravityFlags.CenterHorizontal;
                 case Xamarin.Forms.TextAlignment.End:
                     return GravityFlags.Right;
                 default:
