b345a06cfde9cbb244dbb83e64f9a8cf24d59a93
kamu <kamuxxx@hotmail.com>
2017-07-12 17:25:13 +0900

    Done Time/DatePicker

---
+++ Tests/AiEffects.TestApp/AiEffects.TestApp/ViewModels/AddTimePickerPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Reactive.Bindings;
+using Prism.Services;
+
+namespace AiEffects.TestApp.ViewModels
+{
+    public class AddTimePickerPageViewModel
+    {
+        public ReactiveProperty<bool> EffectOn { get; } = new ReactiveProperty<bool>();
+        public ReactiveProperty<TimeSpan> LabelTime { get; } = new ReactiveProperty<TimeSpan>();
+        public ReactiveProperty<TimeSpan> BoxTime { get; } = new ReactiveProperty<TimeSpan>();
+        public ReactiveProperty<TimeSpan> StackTime { get; } = new ReactiveProperty<TimeSpan>();
+        public ReactiveProperty<TimeSpan> ButtonTime { get; } = new ReactiveProperty<TimeSpan>();
+        public ReactiveProperty<TimeSpan> ImageTime { get; } = new ReactiveProperty<TimeSpan>();
+
+        public ReactiveCommand<TimeSpan> SelectedCommand{get;} = new ReactiveCommand<TimeSpan>();
+
+        public AddTimePickerPageViewModel(IPageDialogService pageDlg)
+        {
+            EffectOn.Value = true;
+
+            LabelTime.Value = TimeSpan.FromHours(3.5);
+            BoxTime.Value = TimeSpan.FromHours(0);
+            StackTime.Value = TimeSpan.FromHours(12.2);
+            ButtonTime.Value = TimeSpan.FromHours(23.0);
+            ImageTime.Value = TimeSpan.FromHours(6.5);
+
+            SelectedCommand.Subscribe(t=>{
+                pageDlg.DisplayAlertAsync("",t.ToString("c"),"OK");
+            });
+        }
+    }
+}
