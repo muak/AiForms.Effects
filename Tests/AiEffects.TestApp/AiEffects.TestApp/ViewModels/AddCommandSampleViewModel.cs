c6c25d80642eafafeba8a3141ef4f688ee4e9869
kamu <kamuxxx@hotmail.com>
2018-02-02 13:19:08 +0900

    AddCommand Short Sample

---
+++ Tests/AiEffects.TestApp/AiEffects.TestApp/ViewModels/AddCommandSampleViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Reactive.Bindings;
+using Prism.Services;
+
+namespace AiEffects.TestApp.ViewModels
+{
+    public class AddCommandSampleViewModel
+    {
+        public ReactiveCommand TapCommand { get; set; }
+        public ReactiveCommand LongCommand { get; set; }
+        public ReactiveProperty<bool> CanExecute { get; } = new ReactiveProperty<bool>(true);
+
+        public AddCommandSampleViewModel(IPageDialogService pageDlg)
+        {
+            TapCommand = CanExecute.ToReactiveCommand();
+            LongCommand = CanExecute.ToReactiveCommand();
+
+            TapCommand.Subscribe(async _ =>{
+                await pageDlg.DisplayAlertAsync("", $"Single Tap in {_}", "OK");
+            });
+
+            LongCommand.Subscribe(async _ => {
+                await pageDlg.DisplayAlertAsync("", $"Long Tap in {_}", "OK");
+            });
+        }
+    }
+}
---
+++ Tests/AiEffects.TestApp/AiEffects.TestApp/Views/AddCommandSample.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AiEffects.TestApp.Views
+{
+    public partial class AddCommandSample : ContentPage
+    {
+        public AddCommandSample()
+        {
+            InitializeComponent();
+        }
+    }
+}
