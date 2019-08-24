db2a26fe0c520408be83a710189c4e34c358dc12
Satoshi Nakamura <kamu@kamusoft-mini-2.local>
2019-08-24 20:46:25 +0900

    Add init method in android

---
+++ AiForms.Effects.Droid/Initialize.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Android.Runtime;
+namespace AiForms.Effects.Droid
+{
+    [Preserve(AllMembers = true)]
+    public static class Initialize
+    {
+        public static void Init() { }
+    }
+}
