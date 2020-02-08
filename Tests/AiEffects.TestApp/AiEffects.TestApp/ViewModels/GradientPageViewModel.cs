3a4ab52f7ff05838a73a3602f5038a1d9520f2f1
Satoshi Nakamura <kamu@kamusoft-mini-2.local>
2020-02-08 15:10:34 +0900

    Add Gradient

---
+++ Tests/AiEffects.TestApp/AiEffects.TestApp/ViewModels/GradientPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using AiForms.Effects;
+using Prism.Mvvm;
+using Reactive.Bindings;
+using Xamarin.Forms;
+
+namespace AiEffects.TestApp.ViewModels
+{
+    public class GradientPageViewModel:BindableBase
+    {
+        public ReactiveProperty<bool> EffectOn { get; } = new ReactiveProperty<bool>(false);
+        public ReactivePropertySlim<GradientColors> Colors { get; } = new ReactivePropertySlim<GradientColors>();
+        public ReactivePropertySlim<GradientOrientation> Orientation { get; } = new ReactivePropertySlim<GradientOrientation>();
+
+        public ReactiveCommand<string> ChangeColorsCommand { get; } = new ReactiveCommand<string>();
+        public ReactiveCommand<string> ChangeOrientationCommand { get; } = new ReactiveCommand<string>();
+
+        GradientColors _colors1 = new GradientColors(new List<Color>
+        {
+            Color.Blue,Color.LightGray
+        });
+        GradientColors _colors2 = new GradientColors(new List<Color>
+        {
+            Color.Red, Color.Green,Color.LightGray
+        });
+
+        public GradientPageViewModel()
+        {
+            Colors.Value = _colors1;
+            Orientation.Value = GradientOrientation.LeftRight;
+
+            ChangeColorsCommand.Subscribe(x =>
+            {
+                var p = int.Parse(x);
+                Colors.Value = p == 2 ? _colors1 : _colors2;
+            });
+
+            ChangeOrientationCommand.Subscribe(x =>
+            {
+                var p = int.Parse(x);
+                Orientation.Value = (GradientOrientation)p;
+            });
+        }
+    }
+}
